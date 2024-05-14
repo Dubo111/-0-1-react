c68928c76695dbf499939e13821245d16a368bae
“jiangbodu” <bo2044208550@qq.com>
2024-05-14 17:12:47 +0800

    fix: 3

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,18 +1,16 @@
 module.exports = {
   extends: ['@commitlint/config-conventional'],
   types: [
-    'feat',
-    'fix',
-    'docs',
-    'style',
-    'refactor',
-    'perf',
-    'test',
-    'build',
-    'ci',
-    'chore',
-    'revert',
-    'wip',
+    'feat', // 新功能 feature
+    'fix', // 修复 bug
+    'docs', // 文档注释
+    'style', // 代码格式(不影响代码运行的变动)
+    'refactor', // 重构(既不增加新功能，也不是修复bug)
+    'perf', // 性能优化
+    'test', // 增加测试
+    'chore', // 构建过程或辅助工具的变动
+    'revert', // 回退
+    'build', // 打包
   ],
   scope: {
     required: false,
